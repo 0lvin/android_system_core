a6187f573b4d1d4bf08f580043f156f6e2c90426
Denis Pauk <pauk.denis@gmail.com>
2015-02-20 23:00:53 +0200

    add new fonts

--- init/screen_log.c
+++ init/screen_log.c
@@ -23,13 +23,13 @@
 #include <linux/fb.h>
 #include <linux/kd.h>
 #include <cutils/klog.h>
-
-#include "font8x8_basic.h"
+#include <string.h>
+#include "font8x8.h"
 
 static unsigned int lx=0, ly=0;
 
 struct FB {
-    unsigned short *bits;
+    unsigned char *bits;
     unsigned size;
     int fd;
     struct fb_fix_screeninfo fi;
@@ -88,7 +88,7 @@ static void fb_update(struct FB *fb)
 
 void vt_create_nodes()
 {
-	int fd;
+    int fd;
     fd = open("/dev/tty0", O_RDWR | O_SYNC);
     if (fd < 0) {
 		mknod("/dev/tty0", 8624, makedev(4, 0));
@@ -112,55 +112,66 @@ static int vt_set_mode(int graphics)
 		klog_write(0, "no tty\n");
         return -1;
     }
-    r = ioctl(fd, KDSETMODE, (void*) (graphics ? KD_GRAPHICS : KD_TEXT));
+    r = ioctl(fd, KDSETMODE, graphics ? KD_GRAPHICS : KD_TEXT);
     close(fd);
     return r;
+}
+
+int set_pixel(struct FB *fb, short r, short g, short b, short x, short y) {
+	long long pixel_color = 0;
+	long red = (r & 0xFFFF) >> (16 - fb->vi.red.length);
+	long green = (g & 0xFFFF) >> (16 - fb->vi.green.length);
+	long blue = (b & 0xFFFF) >> (16 - fb->vi.blue.length);
+	pixel_color =
+		(red << fb->vi.red.offset) |
+		(green << fb->vi.green.offset) |
+		(blue << fb->vi.blue.offset);
+	long count_bytes = fb->vi.bits_per_pixel / 8;
+	char * write_pos = fb->bits + x * count_bytes + y * fb->fi.line_length;
+	memcpy(write_pos, &pixel_color, count_bytes);
 }
 
 int write_text(const char *fn)
 {
     struct FB fb;
-    unsigned short *data, *bits, *ptr;
+    unsigned char *data, *bits, *ptr;
     unsigned count, max;
     int fd;
     unsigned int i, x, y;
     unsigned short value, mask;
 
-	klog_write(0, "screen: %s", fn);
+    klog_write(0, "screen: %s", fn);
 
     if (vt_set_mode(1)) {
-		klog_write(0, "no mode\n");
-        return -1;
+	klog_write(0, "no mode\n");
     }
 
     if (fb_open(&fb))
         goto fail_unmap_data;
 
-    bits = fb.bits;
     for(i = 0; i < strlen(fn); i ++) {
-		if (fn[i] > 0) {
-			if (fn[i] == '\n') {
-				ly += fb_width(&fb) * 8;
-				lx = 0;
-				continue;
-			}
-			if (lx >= (fb_width(&fb) - 8)) {
-				lx = 0;
-				ly += fb_width(&fb) * 8;
-			}
-			if (ly >= (fb_width(&fb) * fb_height(&fb))) {
-				ly = 0;
-			}
-			for (x = 0; x < 8; x++) {
-				for (y = 0; y < 8; y++) {
-					mask = font8x8_basic[fn[i]][y];
-					value = (mask & (1 << x)) == 0 ? 0 : 0xffff;
-					bits[lx + ly + x + y * fb_width(&fb)] = value;
-				}
-			}
-			lx += 8;
+	if (fn[i] == '\n') {
+		ly += 8;
+		lx = 0;
+		continue;
+	}
+	if (lx >= (fb.vi.xres - 8)) {
+		lx = 0;
+		ly += 8;
+	}
+	if (ly >= (fb.vi.yres - 8)) {
+		ly = 0;
+	}
+	for (x = 0; x < 8; x++) {
+		for (y = 0; y < 8; y++) {
+		    mask = font8x8[fn[i] * 8 + y];
+		    /* font pixels: 0 - right, 7 - left */
+		    value = (mask & (1 << (7 - x))) == 0 ? 0 : 0xffff;
+		    set_pixel(&fb, value, value, value, lx + x, ly + y);
 		}
 	}
+	lx += 8;
+    }
     fb_update(&fb);
     fb_close(&fb);
     return 0;
@@ -169,4 +180,3 @@ fail_unmap_data:
     vt_set_mode(0);
     return -1;
 }
-
